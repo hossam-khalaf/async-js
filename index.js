20c76f8a6caf77d85dc7063acc0cee978697b8b2
hossam-khalaf <hossamhassan303089@gmail.com>
2021-05-10 04:15:39 +0200

    callbacks!

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const fs = require('fs')
+
 // callbacks
+
+// setTimeout
+// setTimeout(() => {
+// 	console.log('wait 1 sec')
+// }, 1000)
+
+// nested setTimeouts => callback hell
+// setTimeout(() => {
+// 	console.log(3)
+// 	setTimeout(() => {
+// 		console.log(2)
+// 		setTimeout(() => {
+// 			console.log(1)
+// 		}, 1000)
+// 	}, 1000)
+// }, 1000)
+
+// button event handler in browser
+// const btn
+// btn.addEventListener('click', () => {
+
+// })
+
+// error first callback
+fs.readFile('./test.txt', { encoding: 'utf-8' }, (err, data) => {
+	console.log(data)
+})
 
 // promises
 
